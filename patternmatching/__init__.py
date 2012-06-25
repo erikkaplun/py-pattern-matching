d6c65302cc8818c9051b2a941c94678b62db5ff9
Erik Allik <erik.allik@skype.net>
2012-06-25 19:28:43 +0300

    Added the patternmatching module with the match() function & friends; + tests

---
+++ patternmatching/__init__.py
@@ -0,0 +1,110 @@
+import inspect
+
+
+class _Values(list):
+    pass
+
+
+def match(pattern, data, flatten=True):
+    def _match(pattern, data, success):
+        def _is_ignore(pattern):
+            return not (isinstance(pattern, _Matcher) and not pattern.ignore)
+
+        is_tuple = isinstance(pattern, tuple)
+        if not is_tuple:
+            return (pattern == data if success else False,
+                    data if not _is_ignore(pattern) else NOTHING)
+
+        values = NOTHING
+        data_is_tuple = isinstance(data, tuple)
+
+        for pi in pattern:
+            success, subvalues = _match(pi, data[0] if data_is_tuple and data else None, success)
+            if subvalues is not NOTHING:
+                if values is NOTHING:
+                    values = _Values()
+                if flatten and isinstance(subvalues, _Values):
+                    values += subvalues
+                else:
+                    values.append(tuple(subvalues) if isinstance(subvalues, _Values) else subvalues)
+            data = data[1:] if data_is_tuple and data else None
+        if data:
+            success = False
+
+        return success, values if values is not NOTHING else NOTHING
+
+    ret = _match(pattern, data, True)
+    if flatten and isinstance(ret[1], _Values):
+        return (ret[0],) + tuple(ret[1])
+    else:
+        return ret[0] if ret[1] is NOTHING else (ret[0], tuple(ret[1]) if isinstance(ret[1], _Values) else ret[1])
+
+
+class _Marker(object):
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.name
+
+    def clone(self):
+        return type(self)()
+
+
+class _Matcher(_Marker):
+    ignore = False
+
+    def __req__(self, x):
+        return self.__eq__(x)
+
+
+class _ANY(_Matcher):
+    name = 'ANY'
+
+    def __eq__(self, x):
+        return True
+ANY = _ANY()
+
+
+def IGNORE(x):
+    if isinstance(x, _Matcher):
+        x = x.clone()
+        x.ignore = True
+    return x
+
+
+IGNORE.__eq__ = lambda _: True
+
+
+class IS_INSTANCE(_Matcher):
+    def __init__(self, t):
+        self.t = t
+
+    def __eq__(self, x):
+        return isinstance(x, self.t)
+
+    def __str__(self):
+        return 'IS_INSTANCE(%s)' % self.t
+
+    def clone(self):
+        return type(self)(self.t)
+
+
+class _NOTHING(_Marker):
+    name = 'NOTHING'
+NOTHING = _NOTHING()
+
+
+class Match(_Matcher):
+    def __init__(self, fn):
+        self.fn = fn
+        try:
+            self.name = 'MATCH(%s)' % inspect.getsource(fn).strip()
+        except IOError:
+            self.name = 'MATCH(%s)' % fn
+
+    def __eq__(self, x):
+        return self.fn(x)
+
+    def clone(self):
+        return Match(self.fn)
---
+++ patternmatching/tests.py
@@ -0,0 +1,115 @@
+from contextlib import contextmanager
+
+from patternmatching import match, ANY, IGNORE, NOTHING, IS_INSTANCE
+
+
+FLATTEN = True
+
+
+def NO(pattern, data):
+    x = match(pattern, data)
+    assert isinstance(x, tuple) and not x[0] or not x, x
+
+
+def YES(out, pattern, data):
+    x = match(pattern, data, flatten=FLATTEN)
+    if not FLATTEN:
+        if out is not NOTHING:
+            assert x[0], "should have matched"
+            assert x[1] == out, "should have returned the correct values but returned %s" % repr(x[1])
+        else:
+            assert x
+    else:
+        assert x if out == () else x[0], "should have matched"
+        assert out == () or x[1:] == out, "should have returned the correct values but returned %s" % repr(x[1:])
+
+
+def test_without_flatten():
+    global FLATTEN
+    FLATTEN = False
+
+    NO('foo', 'bar')
+    YES(NOTHING, 'foo', 'foo')
+    NO((), 'whatev')
+    YES(NOTHING, (), ())
+
+    YES('whatev', ANY, 'whatev')
+    YES(('whatev', 'whatev'), ANY, ('whatev', 'whatev'))
+
+    YES(NOTHING, ('foo',), ('foo',))
+    NO(('whatev',), ('whatev', 'whatev'))
+    NO(('whatev', 'whatev'), ('whatev',))
+
+    YES(('foo',), ANY, ('foo',))
+    YES(('foo',), (ANY,), ('foo',))
+    YES(NOTHING, (IGNORE(ANY),), ('whatev',))
+    YES(('foo',), (ANY, IGNORE(ANY)), ('foo', 'whatev',))
+    YES(NOTHING, (IGNORE(ANY), IGNORE(ANY)), ('whatev', 'whatev',))
+    YES(('foo', 'bar'), (ANY, ANY), ('foo', 'bar',))
+
+    YES(NOTHING, ('foo', IGNORE(ANY)), ('foo', 'whatev',))
+    NO(('foo', IGNORE), ('WRONG', 'whatev',))
+    NO(('foo', ANY), ('WRONG', 'whatev',))
+
+    YES(NOTHING, ('foo', (IGNORE(ANY), )), ('foo', ('whatev', )))
+    YES(((1, (2, (3,))), ), ('foo', (ANY, (ANY, (ANY, )))), ('foo', (1, (2, (3,)))))
+    YES((((2, (3,),),),), ('foo', (IGNORE(ANY), (ANY, (ANY, )))), ('foo', (1, (2, (3,)))))
+    YES(((((3,),),),), ('foo', (IGNORE(ANY), (IGNORE(ANY), (ANY, )))), ('foo', (1, (2, (3,)))))
+
+    with assert_not_raises(ValueError):
+        _, ((_, (_, (_,))),) = match(
+            ('foo', (ANY, (ANY, (ANY, )))),
+            ('WRONG', (1, (2, (3,)))),
+            flatten=False
+            )
+
+
+def test_flatten():
+    global FLATTEN
+    FLATTEN = True
+
+    YES((), 'foo', 'foo')
+    YES((), (), ())
+
+    YES(('whatev',), ANY, 'whatev')
+    YES((('whatev', 'whatev'), ), ANY, ('whatev', 'whatev'))
+
+    YES((), ('foo',), ('foo',))
+
+    YES((('foo',),), ANY, ('foo',))
+    YES(('foo',), (ANY,), ('foo',))
+    YES((), (IGNORE(ANY),), ('whatev',))
+    YES(('foo',), (ANY, IGNORE(ANY)), ('foo', 'whatev',))
+    YES((), (IGNORE(ANY), IGNORE(ANY)), ('whatev', 'whatev',))
+    YES(('foo', 'bar'), (ANY, ANY), ('foo', 'bar',))
+
+    YES((), ('foo', IGNORE(ANY)), ('foo', 'whatev',))
+
+    YES((), ('foo', (IGNORE(ANY), )), ('foo', ('whatev', )))
+    YES((1, 2, 3,), ('foo', (ANY, (ANY, (ANY, )))), ('foo', (1, (2, (3,)))))
+    YES((2, 3,), ('foo', (IGNORE(ANY), (ANY, (ANY, )))), ('foo', (1, (2, (3,)))))
+    YES((3,), ('foo', (IGNORE(ANY), (IGNORE(ANY), (ANY, )))), ('foo', (1, (2, (3,)))))
+
+    ok, v1, v2 = match(('foo', ('bar', ANY, ('baz', ANY))),
+                       ('foo', ('bar', 123, ('baz', 456))),
+                       flatten=True)
+    assert ok and (v1, v2) == (123, 456)
+
+
+def test_advanced():
+    global FLATTEN
+    FLATTEN = True
+
+    assert IS_INSTANCE(int) == 1
+    YES((1,), IS_INSTANCE(int), 1)
+    YES((), IGNORE(IS_INSTANCE(int)), 1)
+
+
+@contextmanager
+def assert_not_raises(exc_class=Exception, message=None):
+    if isinstance(exc_class, basestring):
+        message, exc_class = exc_class, Exception
+    try:
+        yield
+    except exc_class as e:
+        raise AssertionError(message or "No exception should have been raised but instead %s was raised" % repr(e))
