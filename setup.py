a75ec0d99550feba9261b553b0f1db2393445fcc
Erik Allik <erik.allik@skype.net>
2012-06-25 19:29:56 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-pattern-matching",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Erik Allik",
+    author_email="eallik@gmail.com",
+)
